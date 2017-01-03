40e09feaca78cf309eaa01c9a4207d3dd4e28420
acn <acn@MacBookCapitanUK-3.local>
2017-01-03 15:46:14 +0100

    POC Travis-Jenkins

---
+++ ios/jenkins.sh
@@ -0,0 +1,6 @@
+set -e
+
+java -jar jenkins-cli.jar -s http://solutions-tokbox.tid.es:8080/ build RegionsTest -s --username "jenkins" --password "vsolutions" | grep ' : SUCCESS' &> /dev/null
+if [ $? != 0 ]; then
+   exit 1
+fi
