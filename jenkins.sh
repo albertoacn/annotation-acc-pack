4dffc56d4de47c920ef658e8beb77e6719d7845a
acn <acn@MacBookCapitanUK-3.local>
2017-01-03 16:37:25 +0100

    Removing info

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@ set -e
 
 curl -O http://solutions-tokbox.tid.es:8080/jnlpJars/jenkins-cli.jar
 
-java -jar jenkins-cli.jar -s http://solutions-tokbox.tid.es:8080/ build RegionsTest -s --username "jenkins" --password "vsolutions" | grep ' : SUCCESS' &> /dev/null
+java -jar jenkins-cli.jar -s http://solutions-tokbox.tid.es:8080/ build RegionsTest -s --username "" --password "" | grep ' : SUCCESS' &> /dev/null
 if [ $? != 0 ]; then
    exit 1
 fi
